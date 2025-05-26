f613a9ac53b8a1cad01cd592660cf9de8bf8bae4
sndryu <shinodaryuuichi1205@outlook.jp>
2025-05-26 19:55:38 +0900

    Update script.js
    Signed-off-by: sndryu <shinodaryuuichi1205@outlook.jp>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   const container = document.getElementById("alert-container");
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 100; i++) {
     const alertBox = document.createElement("div");
     alertBox.className = "alert-window";
     alertBox.style.top = `${Math.random() * 300}px`;
